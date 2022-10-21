2dac416a6795397fb043dcb758455de9eeca1d33
Yoga Ganesha <yganesha@cloudbees.com>
2022-10-20 22:00:52 -0700

    add logs

--- app-code/http-sample.go
+++ app-code/http-sample.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Congratulations! Your Go application has been successfully deployed on Kubernetes.")
+	fmt.Fprintf(w, "Congratulations! Your Go application has been successfully deployed.")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":3000", nil)
+	fmt.Println("Starting basic go application on port 3000")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":3000", nil)
 }
